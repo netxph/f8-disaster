d5bb577980f2ab7aeec9a313e3796682e92cbcd6
Marc Vitalis <marc.vitalis@live.com>
2021-08-25 20:39:10 +0800

    process_data.py

--- src/scripts/process_data.py
+++ src/scripts/process_data.py
@@ -1,16 +1,44 @@
-import sys
-
+import sys, os, pathlib
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
 
+    df = pd.merge(messages, categories, on='id')
+
+    categories = df.categories.str.split(";", expand=True)
+    
+    row = categories.iloc[0]
+    row = row.apply(lambda col: col[:-2])
+    category_colnames = row.to_list()
+
+    categories.columns = category_colnames
+
+    for column in categories:
+    # we need to ensure that no other values than zeroes and ones
+        categories[column] = categories[column].apply(lambda col: col[-1]).apply(lambda col: "0" if col == "0" else "1") 
+    
+        categories[column] = pd.to_numeric(categories[column])
+
+    df = df.drop(columns=["categories"])
+    df = pd.concat([df, categories], axis=1)
+
+    return df
 
 def clean_data(df):
-    pass
+    # drop columns that has only one value
+    one_unique_cols = df.loc[:, df.nunique() == 1].columns.to_list()
+    df = df.drop(columns=one_unique_cols)
 
+    # drop duplicates
+    df = df.drop_duplicates(subset=["id"])
+    df = df[df.message != "#NAME?"]
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(pathlib.Path(os.path.abspath(database_filename)).as_uri())
+    df.to_sql('Message', engine, if_exists="replace", index=False)
 
 
 def main():
