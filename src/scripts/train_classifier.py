8937165cce1d5bfbb61fb1c7779ae28d1155b5d4
Marc Vitalis <marc.vitalis@live.com>
2021-08-26 16:36:24 +0800

    working model

--- src/scripts/train_classifier.py
+++ src/scripts/train_classifier.py
@@ -1,25 +1,125 @@
-import sys
+import sys, os, pathlib
+import pandas as pd
+import re
+import joblib
 
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_split
+
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
+# cache these to speed up tokenization
+lemmatizer = WordNetLemmatizer()
+stop_words = stopwords.words("english")
+pattern = re.compile(r"[^a-zA-Z0-9]")
 
 def load_data(database_filepath):
-    pass
+    path = pathlib.Path(os.path.abspath(database_filepath)).as_uri().replace("file:", "sqlite:")
+    engine = create_engine(path)
+
+    df = pd.read_sql("SELECT * FROM Message", engine)
+
+    GenreType = pd.CategoricalDtype(categories=["news", "direct", "social"], ordered=False)
+    df.genre = df.genre.astype(GenreType)
+
+    X = df[["message", "genre"]]
+    y = df.drop(columns=["id", "message", "genre", "original"])
+    category_names = y.columns
+
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    text = pattern.sub(" ", text.lower())
+    tokens = word_tokenize(text)
 
+    tokens = [lemmatizer.lemmatize(token).strip() for token in tokens if token not in stop_words]
+
+    return tokens
+
+def convert_params(params):
+    dict = {}
+    for key in params:
+        dict[key] = [params[key]]
+
+    return dict
+
+def get_metrics(y_test, y_pred):
+    scores = []
+
+    for i, col in enumerate(y_test.columns):
+        report = classification_report(y_test.iloc[:, i], y_pred[:, i], output_dict=True, zero_division=0)
+
+        scores.append({ 
+            "category": col,
+            "precision_0": report["0"]["precision"],
+            "recall_0": report["0"]["recall"],
+            "f1_0": report["0"]["f1-score"],
+            "support_0": report["0"]["support"],
+            "precision_1": report["1"]["precision"],
+            "recall_1": report["1"]["recall"],
+            "f1_1": report["1"]["f1-score"],
+            "support_1": report["1"]["support"],
+            "accuracy": report["accuracy"],
+            "precision_macro_avg": report["macro avg"]["precision"],
+            "recall_macro_avg": report["macro avg"]["recall"],
+            "f1_macro_avg": report["macro avg"]["f1-score"],
+            "support_macro_avg": report["macro avg"]["support"],
+            "precision_weighted_avg": report["weighted avg"]["precision"],
+            "recall_weighted_avg": report["weighted avg"]["recall"],
+            "f1_weighted_avg": report["weighted avg"]["f1-score"],
+            "support_weighted_avg": report["weighted avg"]["support"]
+        })
+
+    return pd.DataFrame.from_records(scores)
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ("features", ColumnTransformer([
+            ("genre_category", OneHotEncoder(dtype=int), ["genre"]),
+            ("text", Pipeline([
+                ("count", CountVectorizer(tokenizer=tokenize)),
+                ("tfidf", TfidfTransformer())
+            ]), "message")
+        ], remainder="drop")),
+        ("clf", MultiOutputClassifier(MultinomialNB()))
+    ])
 
+    parameters = {
+        "features__text__count__ngram_range": [(1, 1), (1, 2)],
+        "features__text__count__min_df": [1, 10, 20],
+        "clf__estimator": [
+            LogisticRegression(max_iter=1000),
+            LinearSVC(),
+            MultinomialNB()]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=10, n_jobs=8)
+
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    metrics = get_metrics(Y_test, y_pred)
+
+    print(metrics.mean(numeric_only=True))
 
 
 def save_model(model, model_filepath):
-    pass
-
+    joblib.dump(model, model_filepath)
 
 def main():
     if len(sys.argv) == 3:
@@ -33,6 +133,7 @@ def main():
         
         print('Training model...')
         model.fit(X_train, Y_train)
+        print(model.best_params_)
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
